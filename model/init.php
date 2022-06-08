6b91f910da9fc33bbe2ab01c1ae8ba43bb77b1eb
Davi Melo <57837321+DaviiM@users.noreply.github.com>
2022-06-07 22:15:21 -0300

    aula 07 do 06

---
+++ model/init.php
@@ -0,0 +1,11 @@
+<?php
+
+    date_default_timezone_set('America/Sao_Paulo');
+
+    define('BD_SERVIDOR','localhost');
+    define('BD_USUARIO','id18842366_root2');
+    define('BD_SENHA','=HNFbmNh70}Dap33');
+    define('BD_BANCO','id18842366_bdlivraria');
+
+
+?>
---
+++ view/menu.php
@@ -0,0 +1,7 @@
+<hr>
+<div clas="form-group">
+    <div class="mx-auto" stryle="width: 100%">
+        <a href="cadastro.php" class="btn btn-success">Cadastrar</a>
+        <a href="index.php" class="btn btn-default">Consultar</a>
+    </div>
+</div>
