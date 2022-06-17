adf95df7558e632f9151ed35449473fdeedc25b9
Davi Melo <57837321+DaviiM@users.noreply.github.com>
2022-06-16 22:32:21 -0300

    Update banco.php

--- model/banco.php
+++ model/banco.php
@@ -10,5 +10,15 @@
         private function conexao(){
             $this->mysqli = new mysqli(BD_SERVIDOR, BD_USUARIO, BD_SENHA, BD_BANCO);
         }
+        
+        public function setLivro($nome, $autor, $quantidade, $preco, $data){
+            $stmt = $this->mysqli->prepare("INSERT INTO livros ('nome', 'autor', 'quantidade', 'preco', 'data') VALUES (?,?,?,?,?)");
+            $stmt->bind_param("sssss", $nome, $autor, $quantidade, $preco, $data);
+            if($stmt->execute() == TRUE){
+                return true;
+            }else{
+                return false;
+            }
+        }
     }
 ?>
